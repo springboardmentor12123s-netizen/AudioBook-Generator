b1d8fb8f71835cd330431bf59bb31806233c189e
Neha Sai <nehavelpula005@gmail.com>
2025-12-13 23:20:45 +0530

    Update generate_audio.py

--- generate_audio.py
+++ generate_audio.py
@@ -1,6 +1,19 @@
 from gtts import gTTS
+from googletrans import Translator
 
-def create_audio(text, filename="audiobook.mp3"):
-    tts = gTTS(text=text, lang='en')
+def create_audio(text, filename="audiobook.mp3", language="en"):
+    """
+    Convert text to speech in different languages.
+    Automatically translates text before generating audio.
+    """
+
+    translator = Translator()
+
+    # Translate the text to the selected language
+    translated = translator.translate(text, dest=language).text
+
+    # Generate audio in selected language
+    tts = gTTS(text=translated, lang=language)
     tts.save(filename)
+
     return filename
